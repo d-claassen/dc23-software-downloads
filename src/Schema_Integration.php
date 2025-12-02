52d2d7f822822a8ccc16bc5e9e88c736506b6a8b
Dennis Claassen <declaassen@gmail.com>
2025-12-02 09:49:39 +0100

    Update Schema_Integration.php

--- src/Schema_Integration.php
+++ src/Schema_Integration.php
@@ -4,7 +4,7 @@ namespace DC23\SoftwareDownloads;
 
 final class Schema_Integration {
 
-	public function register(): void {{
+	public function register(): void {
 		\add_filter( 'edd_generate_download_structured_data', [ $this, 'filter_download_schema' ] );
 	}
     
