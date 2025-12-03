8fff3accfe5fdf7c561084b779c2181a1a295faf
Dennis Claassen <declaassen@gmail.com>
2025-12-03 22:07:42 +0100

    Update Schema_Integration.php

--- src/Schema_Integration.php
+++ src/Schema_Integration.php
@@ -2,15 +2,20 @@
 
 namespace DC23\SoftwareDownloads;
 
+use function YoastSEO;
+use Yoast\WP\SEO\Premium\Integrations\Third_Party\EDD;
+
 final class Schema_Integration {
 
 	public function register(): void {
 		\add_filter( 'edd_generate_download_structured_data', [ $this, 'filter_download_schema' ] );
         
-        \remove_filter(
-            'wpseo_schema_organization',
-            [ \YoastSEO()->classes->get( \Yoast\WP\SEO\Premium\Integrations\Third_Party\EDD::class ), 'filter_organization_schema' ]
-        );
+        if ( YoastSEO()->classes->has( EDD::class ) ) {
+            \remove_filter(
+                'wpseo_schema_organization',
+                [ YoastSEO()->classes->get( EDD::class ), 'filter_organization_schema' ]
+            );
+        }
 	}
 
 	/**
