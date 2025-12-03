7315ac8dc53b488b0a3de65753624f560984e6f4
Dennis Claassen <declaassen@gmail.com>
2025-12-03 23:11:06 +0100

    Update Schema_Integration.php

--- src/Schema_Integration.php
+++ src/Schema_Integration.php
@@ -2,27 +2,19 @@
 
 namespace DC23\SoftwareDownloads;
 
-use function YoastSEO;
-use function YoastSEOPremium;
-use Yoast\WP\SEO\Premium\Integrations\Third_Party\EDD;
-
 final class Schema_Integration {
 
 	public function register(): void {
 		\add_filter( 'edd_generate_download_structured_data', [ $this, 'filter_download_schema' ] );
-
-		return;
-		try {
-			$edd = YoastSEOPremium()->classes->get( EDD::class ) ?? YoastSEO()->classes->get( EDD::class );
-			\remove_filter(
-				'wpseo_schema_organization',
-				[ $edd, 'filter_organization_schema' ]
-			);
-		} catch(
-			\YoastSEO_Vendor\Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException $serviceNotFound
-		) {
-			// no yoast edd integration
+		\add_filter( 'wpseo_schema_organization', [ $this, 'filter_organization_schema' ] );
+	}
+	
+	public function filter_organization_schema( $organization_piece, $context ) {
+		if ( $context->person_or_organization === 'person' ) {
+			$organization_piece['@type'][] = 'Person';
 		}
+		
+		return $organization_piece;
 	}
 
 	/**
