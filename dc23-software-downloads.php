eaa5fa6024a391a9862e8a3baf7b8ded8c0336b4
Dennis Claassen <declaassen@gmail.com>
2025-12-04 21:11:42 +0100

    Update dc23-software-downloads.php

--- dc23-software-downloads.php
+++ dc23-software-downloads.php
@@ -31,6 +31,7 @@ if ( ! function_exists( 'dc23_software_downloads_setup' ) ) :
         // register_block_type( __DIR__ . '/build/software-downloads' );
 
 		( new \DC23\SoftwareDownloads\Person_Schema_Integration() )->register();
+        ( new \DC23\SoftwareDownloads\Product_Schema_Integration() )->register();
 	}
 endif;
 add_action( 'init', 'dc23_software_downloads_setup' );
