2a2c7f73d5923b430608f71d1ccd4260b534c671
Dennis Claassen <declaassen@gmail.com>
2025-12-04 20:59:12 +0100

    Scope specific Person_Schema changes

--- dc23-software-downloads.php
+++ dc23-software-downloads.php
@@ -30,7 +30,7 @@ if ( ! function_exists( 'dc23_software_downloads_setup' ) ) :
 	function dc23_software_downloads_setup(): void {
         // register_block_type( __DIR__ . '/build/software-downloads' );
 
-		( new \DC23\SoftwareDownloads\Schema_Integration() )->register();
+		( new \DC23\SoftwareDownloads\Person_Schema_Integration() )->register();
 	}
 endif;
 add_action( 'init', 'dc23_software_downloads_setup' );
