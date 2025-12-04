8ec4d9d494556e7a9a6f59d2ae182b992b6d9684
d-claassen <d-claassen@users.noreply.github.com>
2025-12-04 13:33:28 +0000

    0.3.1

--- dc23-software-downloads.php
+++ dc23-software-downloads.php
@@ -7,7 +7,7 @@
  *
  * @wordpress-plugin
  * Plugin Name: DC23 Software Downloads
- * Version: 0.3.0
+ * Version: 0.3.1
  * Description: Add structured markup related to Easy Digital Downloads in your WordPress website.
  * Requires at least: 6.2
  * Requires PHP: 8.1
