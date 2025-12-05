63c3003e05d22dd29cde080e7843c7428241456c
Dennis Claassen <declaassen@gmail.com>
2025-12-05 15:28:48 +0100

    Update Product_Schema_IntegrationTest.php

--- tests/Product_Schema_IntegrationTest.php
+++ tests/Product_Schema_IntegrationTest.php
@@ -28,22 +28,14 @@ class Product_Schema_IntegrationTest extends \WP_UnitTestCase {
 	
 	private function get_post_content(): string {
 		return <<<'EOL'
-				Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live 
-				the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large 
-				language ocean. A small river named Duden flows by their place and supplies it with the necessary
-				regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.
-				Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic
-				life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for
-				the far World of Grammar. The Big Oxmox advised her not to do so, because there were thousands
-				of bad Commas, wild Question Marks and devious Semikoli, but the Little Blind Text didn’t
-				listen. She packed her seven versalia, put her initial into the belt and made herself on the
-				way. When she reached the first hills of the Italic Mountains, she had a last view back on
-				the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline
-				of her own road, the Line Lane. Pityful a rethoric question ran over her cheek, then she
-				continued her way. On her way she met a copy. The copy warned the Little Blind Text, that
-				where it came from it would have been rewritten a thousand times and everything that was left
-				from its origin would be the word "and".
-				EOL;
+			<!-- wp:heading -->
+			<h1 class="wp-block-heading">Software downloads</h3>
+			<!-- /wp:heading -->
+
+			<!-- wp:yoast/faq-block {"questions":[{"id":"faq-question-1764935399988","question":["What WordPress plugins do I need?"],"answer":["Yoast SEO and Easy Digital Downloads."],"jsonQuestion":"What WordPress plugins do I need?","jsonAnswer":"Yoast SEO and Easy Digital Downloads."}]} -->
+			<div class="schema-faq wp-block-yoast-faq-block"><div class="schema-faq-section" id="faq-question-1764935399988"><strong class="schema-faq-question">What WordPress plugins do I need?</strong> <p class="schema-faq-answer">Yoast SEO and Easy Digital Downloads.</p> </div> </div>
+			<!-- /wp:yoast/faq-block -->
+			EOL;
 	}
 
 	public function test_should_type_download_webpage_as_itempage(): void {
