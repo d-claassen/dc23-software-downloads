0ef096c504578e344b347f884d40c7e6610b4256
Dennis Claassen <declaassen@gmail.com>
2025-12-05 06:46:44 +0100

    Optimize Product schema markup (#5)

---
+++ src/Product_Schema_Integration.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace DC23\SoftwareDownloads;
+
+final class Product_Schema_Integration {
+
+    public function register(): void {
+        \add_filter( 'wpseo_schema_webpage', [ $this, 'filter_webpage_schema' ], 20, 2 );
+    }
+
+	/**
+	 * Product schema graph.
+	 *
+	 * @param array<string, mixed> $webpage_piece
+	 * @param WPSEO_Schema_Context $context
+	 *
+	 * @return array<string, mixed>
+	 */
+	public function filter_webpage_schema( $webpage_piece, $context ) {
+		if ( ! is_singular( 'download' ) ) {
+			return $webpage_piece;
+		}
+
+		// Ensure the download page is an ItemPage.
+		if ( ! in_array( 'ItemPage', (array)	$webpage_piece['@type']) ) {
+			$webpage_piece['@type'] = (array) $webpage_piece['@type'];
+			$webpage_piece['@type'][] = 'ItemPage';
+		}
+
+		// We normally add a `ReadAction` on pages, we're replacing with a `BuyAction` on product pages.
+		$webpage_piece['potentialAction'] = [
+			'@type'  => 'BuyAction',
+			'target' => $context->canonical, // \YoastSEO()->meta->for_current_page()->canonical,
+		];
+		
+		unset( $webpage_piece['datePublished'], $webpage_piece['dateModified'] );
+		
+		return $webpage_piece;
+	}
+}
