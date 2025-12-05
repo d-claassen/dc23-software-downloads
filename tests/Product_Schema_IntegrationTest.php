b911729bae2c56898ed988532f745298babec4cc
Dennis Claassen <declaassen@gmail.com>
2025-12-05 01:13:35 +0100

    Update Product_Schema_IntegrationTest.php

--- tests/Product_Schema_IntegrationTest.php
+++ tests/Product_Schema_IntegrationTest.php
@@ -64,12 +64,12 @@ class Product_Schema_IntegrationTest extends \WP_UnitTestCase {
 		$this->assertJson( $yoast_schema, 'Yoast schema should be valid JSON' );
 		$yoast_schema_data = \json_decode( $yoast_schema, JSON_OBJECT_AS_ARRAY );
 
-		$edd_schema = $this->get_edd_schema_output();
-		$this->assertJson( $edd_schema, 'EDD schema should be valid JSON' );
-		$edd_schema_data = \json_decode( $edd_schema, JSON_OBJECT_AS_ARRAY );
+		// $edd_schema = $this->get_edd_schema_output();
+		// $this->assertJson( $edd_schema, 'EDD schema should be valid JSON' );
+		// $edd_schema_data = \json_decode( $edd_schema, JSON_OBJECT_AS_ARRAY );
 
 		$webpage_piece  = $this->get_piece_by_type( $yoast_schema_data['@graph'], 'ItemPage' );
-		$product_piece = $this->get_piece_by_type( $edd_schema_data, 'Product' );
+		// $product_piece = $this->get_piece_by_type( $edd_schema_data, 'Product' );
 
 		$this->assertSame( 
 			['WebPage', 'ItemPage' ],
@@ -111,7 +111,7 @@ class Product_Schema_IntegrationTest extends \WP_UnitTestCase {
 		$webpage_piece  = $this->get_piece_by_type( $yoast_schema_data['@graph'], 'ItemPage' );
 
 		$this->assertSame( 
-            ['ItemPage'],
+            ['WebPage', 'ItemPage'],
             $webpage_piece['@type'],
 			'webpage piece should be typed additionally with ItemPage'
 		);
