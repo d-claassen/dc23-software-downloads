37b96a1b9b3a438bc9361acb501502880a74e04d
Dennis Claassen <declaassen@gmail.com>
2025-12-06 01:36:56 +0100

    Update Product_Schema_Integration.php

--- src/Product_Schema_Integration.php
+++ src/Product_Schema_Integration.php
@@ -34,6 +34,18 @@ final class Product_Schema_Integration {
 			}
 
 			$missing_entities = array_diff( $webpage_piece[ 'mainEntity' ], $context->main_entity_of_page );
+			$missing_entities = array_filter(
+				$context->main_entity_of_page,
+				static fn ( $entity ): bool {
+					foreach( $webpage_piece[ 'mainEntity' ] as $assigned_entity ) {
+						if( $entity === $assigned_entity ) {
+						 // false: not missing.
+							return false;
+						}
+					}
+					return true:
+				}
+			);
 
 			print '<!-- context main entity: ';
 			var_dump( $context->main_entity_of_page );
