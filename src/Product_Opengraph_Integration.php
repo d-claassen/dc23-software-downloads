758a8178536fb01134ec57da733b0ef0e68ff0c3
Dennis Claassen <declaassen@gmail.com>
2025-12-06 20:48:44 +0100

    Change og:type

--- src/Product_Opengraph_Integration.php
+++ src/Product_Opengraph_Integration.php
@@ -5,6 +5,21 @@ namespace DC23\SoftwareDownloads;
 final class Product_Opengraph_Integration {
 
     public function register(): void {
-        
-    }
+        add_filter( 'wpseo_opengraph_type', [ $this, 'download_type_product' ] );
+	}
+
+	/**
+	 * Return 'product' when current post is an EDD download.
+	 *
+	 * @param string $type
+	 *
+	 * @return string
+	 */
+	public function download_type_product( $type ) {
+		if ( is_singular( 'download' ) ) {
+			return 'product';
+		}
+
+		return $type;
+	}
 }
