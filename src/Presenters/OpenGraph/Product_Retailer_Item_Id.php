f4a665832402921533fee4de75a0e1ff08c1eb8d
Dennis Claassen <declaassen@gmail.com>
2025-12-07 03:09:06 +0100

    Update Product_Retailer_Item_Id.php

--- src/Presenters/OpenGraph/Product_Retailer_Item_Id.php
+++ src/Presenters/OpenGraph/Product_Retailer_Item_Id.php
@@ -1,10 +1,18 @@
 <?php
 
-namespace DC23\SoftwareDownloads\Presenters\OpenGraph
-/**
- * Represents the product's retailer item ID.
- */
-class Product_Retailer_Item_ID extends WPSEO_WooCommerce_Abstract_Product_Presenter {
+namespace DC23\SoftwareDownloads\Presenters\OpenGraph;
+
+use Yoast\WP\SEO\Presenters\Abstract_Indexable_Tag_Presenter;
+
+final class Product_Retailer_Item_ID extends Abstract_Indexable_Tag_Presenter {
+
+	/**
+	 * The tag format including placeholders.
+	 *
+	 * @var string
+	 */
+	protected $tag_format = self::META_PROPERTY_CONTENT;
+
 
 	/**
 	 * The tag key name.
