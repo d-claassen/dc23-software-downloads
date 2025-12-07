d5a91c08635fa1eeef29715c5f4ee4071df45ab1
Dennis Claassen <declaassen@gmail.com>
2025-12-07 02:15:23 +0100

    Update Product_Brand.php

--- src/Presenters/OpenGraph/Product_Brand.php
+++ src/Presenters/OpenGraph/Product_Brand.php
@@ -6,4 +6,12 @@ use Yoast\WP\SEO\Presenters\Abstract_Indexable_Tag_Presenter;
 
 final class Product_Brand extends Abstract_Indexable_Tag_Presenter {
 
+	/**
+	 * The tag format including placeholders.
+	 *
+	 * @var string
+	 */
+	protected $tag_format = self::META_PROPERTY_CONTENT;
+
+
 }
