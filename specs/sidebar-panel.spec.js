5bd5288c7660a20586b774844d3eda778b19c825
Dennis Claassen <declaassen@gmail.com>
2025-12-30 23:00:48 +0100

    Create sidebar-panel.spec.js

---
+++ specs/sidebar-panel.spec.js
@@ -0,0 +1,64 @@
+/**
+ * WordPress dependencies
+ */
+const { test, expect } = require( '@wordpress/e2e-test-utils-playwright' );
+
+test.describe( 'Sidebar panel', () => {
+	test.afterEach( async ( { requestUtils } ) => {
+		await requestUtils.deleteAllPages();
+	} );
+
+	test.describe( 'panel per post type', () => {
+		test( 'it shows for `downloads`', async ( {
+			page,
+			admin,
+			editor,
+		} ) => {
+			await admin.createNewPost( {
+				title: 'Download',
+				postType: 'download',
+				status: 'publish',
+			} );
+			await editor.openDocumentSettingsSidebar();
+			await expect(
+				page.getByRole( 'button', { name: 'Software Downloads' } )
+			).toBeVisible();
+		} );
+
+		test( 'invisible for regular posts', async ( {
+			page,
+			admin,
+			editor,
+			requestUtils,
+		} ) => {
+			const newPage = await requestUtils.createPost( {
+				title: 'Post',
+				status: 'publish',
+			} );
+			await admin.editPost( newPage.id );
+			await editor.openDocumentSettingsSidebar();
+			await expect(
+				page.getByRole( 'button', { name: 'Software Downloads' } )
+			).toBeHidden();
+		} );
+
+		test.fixme(
+			'invisible in site editor',
+			async ( { page, editor, admin, requestUtils } ) => {
+				const newPage = await requestUtils.createPage( {
+					title: 'Posts Page',
+					status: 'publish',
+				} );
+				await admin.visitSiteEditor( {
+					postId: newPage.id,
+					postType: 'page',
+					canvas: 'edit',
+				} );
+				await editor.openDocumentSettingsSidebar();
+				await expect(
+					page.getByRole( 'button', { name: 'Software Downloads' } )
+				).toBeHidden();
+			}
+		);
+	} );
+} );
