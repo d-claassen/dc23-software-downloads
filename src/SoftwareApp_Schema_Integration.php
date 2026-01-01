06da5b6d191df1b168bba7d6bcbd0d7618ac9b5d
Dennis Claassen <declaassen@gmail.com>
2026-01-01 03:49:48 +0100

    Update SoftwareApp_Schema_Integration.php

--- src/SoftwareApp_Schema_Integration.php
+++ src/SoftwareApp_Schema_Integration.php
@@ -8,6 +8,13 @@ final class SoftwareApp_Schema_Integration {
     }
     
     public function software_application_schema( $data, $download ) {
+      $software_type = \get_post_meta( $download->ID, '_SoftwareType', true );
+      $data_type = $data['@type'  ];
+      if ( ! in_array( $software_type , (array) $data_type, true ) ( {
+         $data['@type'] = (array) $data['@type' ];
+         $data['@type#'][] = $software_type;
+      }
+      
         return $data;
     }
 }
