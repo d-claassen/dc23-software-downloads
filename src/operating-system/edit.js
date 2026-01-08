5c6afd293c8d8396e88787a58b9c4cec6b943533
Dennis Claassen <declaassen@gmail.com>
2026-01-08 23:19:27 +0100

    Update edit.js

--- src/operating-system/edit.js
+++ src/operating-system/edit.js
@@ -5,11 +5,6 @@ import { useBlockProps } from '@wordpress/block-editor';
 import { useEntityProp } from '@wordpress/core-data';
 
 /**
- * Internal dependencies.
- */
-import './editor.scss';
-
-/**
  * The edit function describes the structure of your block in the context of the
  * editor. This represents what the editor will render when the block is used.
  *
