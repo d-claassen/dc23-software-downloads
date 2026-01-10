43f81fbb38f9c089aebd41bb2aace7f5633802c3
Dennis Claassen <declaassen@gmail.com>
2026-01-10 07:13:09 +0100

    Update render.php

--- src/application-category/render.php
+++ src/application-category/render.php
@@ -1,15 +1,6 @@
 <?php
-/**
- * PHP file to use when rendering the block type on the server to show on the front end.
- *
- * The following variables are exposed to the file:
- *     $attributes (array): The block attributes.
- *     $content (string): The block default content.
- *     $block (WP_Block): The block instance.
- *
- * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
- */
+$category = get_post_meta( $block->context['postId'], '_dc23_software_category', true );
 ?>
-<p <?php echo get_block_wrapper_attributes(); ?>>
-	<?php esc_html_e( 'Application category – hello from a dynamic block!', 'application-category' ); ?>
-</p>
+<div <?php echo get_block_wrapper_attributes(); ?>>
+	<?php echo esc_html( $category ); ?>
+</div>
